710eb337e648968758ff38d8e1c4a5d04f7fc72a
«Yana <CvTT135@gmail.com>
2023-02-25 14:57:40 +0300

    Add Example 011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection)  
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)      // void метод ничего не возвращает, 
+                                //отсутствует "return"
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine (col[position]);
+        position++;
+    }
+}
+
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;  // если элемента нет - значение будет (-1)
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // фиксирует первую позицию индекса
+        }
+        index++;
+    }
+    return position;
+} 
+
+int[] array = new int[10];         // массив создается с нулями
+
+
+FillArray(array);               // метод, который заполнил массив
+PrintArray(array);              // метод, который распечатал массив
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
